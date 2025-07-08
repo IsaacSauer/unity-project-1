51bca051d245827ce87ef2c5e3168f3c419ef63d
Isaac Sauer <isaacsauer@icloud.com>
2025-07-08 21:46:43 +0200

    Unity Project

---
+++ unity-project/Assets/Scripts/Room.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class Room : MonoBehaviour
+    {
+        #region Editor Fields
+        
+        
+        
+        #endregion
+    }
+}
---
+++ unity-project/Assets/Scripts/RoomSwitcher.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class RoomSwitcher : MonoBehaviour
+    {
+        #region Editor Fields
+
+        private List<Room> _rooms = new();
+
+        #endregion
+
+        #region Methods
+        
+        
+        
+        #endregion
+    }
+}
