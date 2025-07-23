1f3888d4e1c9588d46ec7f638f551e5c7859783f
Isaac Sauer <isaac.sauer@triangle-factory.be>
2025-07-23 18:50:55 +0200

    some build fixes

--- unity-project/Assets/Scripts/LoadScene.cs
+++ unity-project/Assets/Scripts/LoadScene.cs
@@ -1,4 +1,7 @@
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,11 +9,19 @@ namespace Game
 {
 	public class LoadScene : MonoBehaviour
 	{
+		[SerializeField] private string _sceneName;
+
+#if UNITY_EDITOR
 		[SerializeField] private SceneAsset _startScene;
+		private void OnValidate()
+		{
+			_sceneName = _startScene.name;
+		}
+#endif
 
 		private void Start()
 		{
-			Invoker.InvokeDelayed(() => SceneManager.LoadScene(_startScene.name), 1f);
+			Invoker.InvokeDelayed(() => SceneManager.LoadScene(_sceneName), 1f);
 		}
 	}
 }
